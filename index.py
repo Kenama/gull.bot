733fed3d7009cad58ded486921f4ba91855a52ff
Kenama <94038374+Kenama@users.noreply.github.com>
2021-11-10 09:50:56 +0200

    Update index.py

--- index.py
+++ index.py
@@ -14,14 +14,14 @@ print('Бот запущен, теперь ты - Долбоёб')
 
 @client.on_message(filters.regex('Я еблан|я еблан') & filters.me)
 def ghoul_spam_handler(client, message):
-    i = 1000
+    i = 10000
     while i > 0:
         try:
-            client.send_message(message.chat.id, f'{i} - 7 = {i-7}')
+            client.send_message(message.chat.id, f'{i} - 2 = {i-2}')
         except FloodWait as e:
             sleep(e.x)
 
-        i -= 7
+        i -= 2
         sleep(1/messages_per_second)        
 
     if(end_message != ''):
@@ -30,18 +30,18 @@ def ghoul_spam_handler(client, message):
 
 @client.on_message(filters.command(ghoul_table_command, prefixes=command_prefixes) & filters.me)
 def ghoul_table_handler(client, message):
-    i = 1000
+    i = 10000
     while i > 62:
         try:
-            text = f'{i} - 7 = {i-7}'
+            text = f'{i} - 2 = {i-2}'
             for j in range(1,10):
-                text += f'\n{i-7*j} - 7 = {i-7*(j+1)}'
+                text += f'\n{i-2*j} - 2 = {i-2*(j+1)}'
             message.edit_text(f'`{text}`')
             sleep(sleep_time_ghoul)
         except FloodWait as e:
             sleep(e.x)
 
-        i -= 7
+        i -= 2
 
     if(end_message != ''):
         client.send_message(message.chat.id, end_message)
